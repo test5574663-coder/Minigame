36600da386f30c1325836728589cf1cd670348b4
test5574663-coder <test5574663@gmail.com>
2026-03-17 10:35:08 +0700

    Update fishList.js

--- fishList.js
+++ fishList.js
@@ -1,52 +1,52 @@
 module.exports = {
 
 common: [
-{ name:"Cá rô", value:10 },
-{ name:"Cá trê", value:10 },
-{ name:"Dép lào thủng lỗ", value:5 },
-{ name:"Bịch rác mạ vàng", value:5 },
-{ name:"Quần sịp ố vàng", value:5 },
-{ name:"Sắt vụn", value:20 },
-{ name:"Cá lau kính", value:5 }
+{ name:"Cá rô", value:1000 },
+{ name:"Cá trê", value:1000 },
+{ name:"Dép lào thủng lỗ", value:50 },
+{ name:"Bịch rác mạ vàng", value:50 },
+{ name:"Quần sịp ố vàng", value:50 },
+{ name:"Sắt vụn", value:2000 },
+{ name:"Cá lau kính", value:2000 }
 ],
 
 uncommon: [
-{ name:"Cá chép", value:20 },
-{ name:"Cá lóc", value:30 },
-{ name:"Nhựa tái chế", value:25 },
-{ name:"Xu bạc", value:50 },
-{ name:"Quần què", value:20 },
-{ name:"Khúc gỗ mục", value:5 }
+{ name:"Cá chép", value:2000 },
+{ name:"Cá lóc", value:3000 },
+{ name:"Nhựa tái chế", value:250 },
+{ name:"Xu bạc", value:5000 },
+{ name:"Quần què", value:200 },
+{ name:"Khúc gỗ mục", value:500 }
 ],
 
 rare: [
-{ name:"Cá trắm", value:40 },
-{ name:"Xu cổ", value:60 },
-{ name:"Cá ngừ", value:55 },
-{ name:"Cá kiếm", value:70 },
-{ name:"Răng vàng", value:100 }
+{ name:"Cá trắm", value:4000 },
+{ name:"Xu cổ", value:6000 },
+{ name:"Cá ngừ", value:5500 },
+{ name:"Cá kiếm", value:7000 },
+{ name:"Răng vàng", value:20000 }
 ],
 
 epic: [
-{ name:"Cá hồi", value:80 },
-{ name:"Tôm hùm", value:100 },
-{ name:"Cua hoàng đế", value:120 },
-{ name:"Rương kho báu", value:150 }
+{ name:"Cá hồi", value:8000 },
+{ name:"Tôm hùm", value:60000 },
+{ name:"Cua hoàng đế", value:65000 },
+{ name:"Rương kho báu", value:150000 }
 ],
 
 legendary: [
-{ name:"Cá mập", value:180 },
-{ name:"Cá mập Megalodon", value:220 },
-{ name:"Kho báu hải tặc", value:250 },
-{ name:"Cá voi xanh", value:200 },
-{ name:"Cá voi sát thủ", value:210 }
+{ name:"Cá mập", value:18000 },
+{ name:"Cá mập Megalodon", value:22000 },
+{ name:"Kho báu hải tặc", value:250000 },
+{ name:"Cá voi xanh", value:200000 },
+{ name:"Cá voi sát thủ", value:190000 }
 ],
 
 mythic: [
-{ name:"Kraken", value:2 },
-{ name:"Leviathan", value:3 },
-{ name:"Thủy Long", value:4 },
-{ name:"Rùa vàng Kim Quy", value:5 }
+{ name:"Kraken", value:500000 },
+{ name:"Leviathan", value:700000 },
+{ name:"Thủy Long", value:850000 },
+{ name:"Rùa vàng Kim Quy", value:1000000 }
 ]
 
 }

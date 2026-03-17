08675b28cf296a9055cb3c460b6ce237c7537fbe
test5574663-coder <test5574663@gmail.com>
2026-03-17 10:31:18 +0700

    Add files via upload

---
+++ fishList.js
@@ -0,0 +1,52 @@
+module.exports = {
+
+common: [
+{ name:"Cá rô", value:10 },
+{ name:"Cá trê", value:10 },
+{ name:"Dép lào thủng lỗ", value:5 },
+{ name:"Bịch rác mạ vàng", value:5 },
+{ name:"Quần sịp ố vàng", value:5 },
+{ name:"Sắt vụn", value:20 },
+{ name:"Cá lau kính", value:5 }
+],
+
+uncommon: [
+{ name:"Cá chép", value:20 },
+{ name:"Cá lóc", value:30 },
+{ name:"Nhựa tái chế", value:25 },
+{ name:"Xu bạc", value:50 },
+{ name:"Quần què", value:20 },
+{ name:"Khúc gỗ mục", value:5 }
+],
+
+rare: [
+{ name:"Cá trắm", value:40 },
+{ name:"Xu cổ", value:60 },
+{ name:"Cá ngừ", value:55 },
+{ name:"Cá kiếm", value:70 },
+{ name:"Răng vàng", value:100 }
+],
+
+epic: [
+{ name:"Cá hồi", value:80 },
+{ name:"Tôm hùm", value:100 },
+{ name:"Cua hoàng đế", value:120 },
+{ name:"Rương kho báu", value:150 }
+],
+
+legendary: [
+{ name:"Cá mập", value:180 },
+{ name:"Cá mập Megalodon", value:220 },
+{ name:"Kho báu hải tặc", value:250 },
+{ name:"Cá voi xanh", value:200 },
+{ name:"Cá voi sát thủ", value:210 }
+],
+
+mythic: [
+{ name:"Kraken", value:2 },
+{ name:"Leviathan", value:3 },
+{ name:"Thủy Long", value:4 },
+{ name:"Rùa vàng Kim Quy", value:5 }
+]
+
+}
